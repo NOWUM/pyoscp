a4c2f1890789a128d2766f33cc7a3623aa005f06
Tobias Grüll <gruell@fh-aachen.de>
2021-05-06 13:42:26 +0200

    added unit 'EUR/KWH'

--- oscp/json_models.py
+++ oscp/json_models.py
@@ -12,7 +12,7 @@ from flask_restx import fields, Model
 
 energy_measurement_unit = ['WH', 'KWH']
 instantaneous_measurement_unit = ['A', 'W', 'KW', 'WH', 'KWH']
-forecasted_block_unit = ['A', 'W', 'KW', 'WH', 'KWH', 'EUR']
+forecasted_block_unit = ['A', 'W', 'KW', 'WH', 'KWH', 'EUR', 'EUR/KWH']
 energy_flow_direction = ['NET', 'IMPORT', 'EXPORT']
 energy_type = ['FLEXIBLE', 'NONFLEXIBLE', 'TOTAL']
 measurement_configuration = ['CONTINUOUS', 'INTERMITTENT']
